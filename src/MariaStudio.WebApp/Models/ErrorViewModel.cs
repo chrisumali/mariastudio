4d378cc24c1fe437813b677126fcc1b1f611ed2f
Chris Umali <chrisumali.work@gmail.com>
2026-09-24 16:04:29 +0800

    initial create

---
+++ src/MariaStudio.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MariaStudio.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
